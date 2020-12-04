08687a39cc7778cf85ed9029dad4771dabead41e
xukwei0516 <260724109@qq.com>
2020-12-04 16:15:09 +0800

    第一次提交

---
+++ src/main/java/controller/HelloServlet.java
@@ -0,0 +1,38 @@
+package controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.lang.ref.ReferenceQueue;
+
+/**
+ * @ClassName HelloServlet
+ * @Description TODO
+ * @Author Administrator
+ * @Date: 2020/12/3 10:35
+ * @Version 1.0
+ */
+@WebServlet("/hello.do")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String serverName = req.getServerName();
+        int serverPort = req.getServerPort();
+        String remoteHost = req.getRemoteHost();
+        int remotePort = req.getRemotePort();
+        String localName = req.getLocalName();
+        int localPort = req.getLocalPort();
+        req.setAttribute("ip", serverName);
+        req.setAttribute("port",serverPort);
+        req.setAttribute("remoteHost", remoteHost);
+        req.setAttribute("remotePort", remotePort);
+        req.setAttribute("localName", localName);
+        req.setAttribute("localPort", localPort);
+       
+        req.getRequestDispatcher("/index.jsp").forward(req, resp);
+
+    }
+}
